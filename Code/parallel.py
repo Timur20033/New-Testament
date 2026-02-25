2babb3aa1efb57548468d952e581448a67a49960
Timur <timurorigami@mail.ru>
2026-02-25 21:44:05 +0300

    Konabere-Russian-French corpus added

--- Code/parallel.py
+++ Code/parallel.py
@@ -2,14 +2,51 @@ import re
 
 
 def parallel(text_rus, text_kon):
+
     lines_rus = re.findall(r'\d+ (?:[^\d\n]+\n)+', text_rus)
     lines_kon = re.findall(r'\d+ (?:[^\d\n]+\n)+', text_kon)
+
     if len(lines_kon) == len(lines_rus):
         for idx, line_kon in enumerate(lines_kon):
             line_rus = lines_rus[idx]
             line_rus = re.sub('\d+', '', line_rus).strip()
             repl = line_kon + line_rus + '\n\n'
             text_kon = text_kon.replace(line_kon, repl, 1)
+
         return text_kon
+    
     else:
+
         return f'{len(lines_rus)} != {len(lines_kon)}'
+    
+
+def highlight_rus(par_text):
+
+    lines_rus_kon = re.findall(r'\d+ (?:[^\d\n]+\n)+', par_text)
+
+    for line in lines_rus_kon:
+        sublines = line.strip().split('\n')
+        kon = '\n'.join(sublines[:-1])
+        rus = sublines[-1].strip()
+        replacement = f'{kon}\n<{rus}>\n'
+        par_text = par_text.replace(line, replacement)
+
+    return par_text
+
+
+def add_fr(par_text, fr_text):
+
+    lines_ru_kon = re.findall(r'\d+ (?:[^\d\n]+\n)+', par_text)
+    lines_fr = re.findall(r'\d+ (?:[^\d\n]+\n)+', fr_text)
+
+    if len(lines_ru_kon) == len(lines_fr):
+        for idx, line_ru_kon in enumerate(lines_ru_kon):
+            line_fr = re.sub('\d+ (.+)', r'\1', lines_fr[idx])
+            replacement = re.sub('>', f'\n{line_fr.strip()}>', line_ru_kon)
+            par_text = par_text.replace(line_ru_kon, replacement, 1)
+
+        return par_text
+    
+    else:
+
+        return f'{len(lines_ru_kon)} != {len(lines_fr)}'
