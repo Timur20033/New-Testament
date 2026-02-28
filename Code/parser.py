80863a4330a892377bbe5b3d266e868cd9f5e075
Timur <timurorigami@mail.ru>
2026-02-28 19:44:29 +0300

    NdeN constructions extracted better

--- Code/parser.py
+++ Code/parser.py
@@ -4,42 +4,114 @@ from collections import Counter
 from nltk.corpus import wordnet
 
 
-def extract_verses(text):
+def extract_verses(text: str):
+
     verses = re.findall(r'\d+[^\d]+>', text)
     kon_ru_fr = {verse: re.findall(r'(.+)>', verse)[0] for verse in verses}
+
     return kon_ru_fr
 
 
-def parse_verses(kon_ru_fr):
+def parse_verses(kon_ru_fr: dict):
     nlp = spacy.load("fr_core_news_sm")
     return[nlp(verse) for verse in kon_ru_fr.values()]
 
 
-def filter_verses(kon_ru_fr, filter):
-    filtered_verses = []
+def filter_verses(kon_ru_fr: dict, target: list):
+
+    filtered = []
     for k, v in kon_ru_fr.items():
-        if v in filter:
-            filtered_verses.append(k)
-    return filtered_verses
+        if v in target:
+            highl_verse = k
+            for constr in target[v]:
+                highl_verse = highl_verse.replace(constr, constr.upper())
+            filtered.append(highl_verse)
+
+    return filtered
 
 
-def is_concrete(lemma, pos, lang):
+def is_concrete(lemma: str):
+
     hypernyms = []
-    synsets = wordnet.synsets(lemma, pos=pos, lang=lang)
+    synsets = wordnet.synsets(lemma, pos='n', lang='fra')
+
     for synset in synsets:
         for path in synset.hypernym_paths():
             hypernyms.append(path[2].name().split('.')[0])
+
     if not hypernyms:
         return None
+    
     else:
         freq_sense = Counter(hypernyms).most_common(1)[0][0]
+
         if freq_sense == 'object':
             return True
+        
         else:
             return False
+        
+
+def build_NdeN_nref(w1: str, 
+                    w2: str, 
+                    w3: str, 
+                    w4: str):
+
+    if not w4:
+        constr = f'{w1} {w2} {w3}'
+        if "d'" in constr:
+            constr = constr.replace("d' ", "d'")
+
+    else:
+        constr = f'{w1} {w2}{w3} {w4}'
+    
+    return constr
 
 
-def get_cop_det_nref(text):
+def is_NdeN_nref(w1, w2, w3, w4):
+
+    if (w1.pos_ == 'NOUN'  
+        and w2.text in ['de', "d'"] 
+        and w3.pos_ == 'NOUN'):
+        if is_concrete(w1.lemma_) and is_concrete(w3.lemma_):
+            constr = build_NdeN_nref(w1.text, w2.text, w3.text, None)
+
+            return True, constr
+
+    elif (w1.pos_ == 'NOUN' 
+          and w2.text in ['de', "d'"] 
+          and w3.lemma_ == 'un' 
+          and w4.pos_ == 'NOUN'):
+        if is_concrete(w1.lemma_) and is_concrete(w4.lemma_):
+            constr = build_NdeN_nref(w1.text, w2.text, w3.text, w4.text)
+    
+            return True, constr 
+    
+    return False, None
+
+
+def get_NdeN_nref(text: str):
+
+    kon_ru_fr = extract_verses(text)
+    kon_ru_fr = {k: v for k, v in kon_ru_fr.items() if re.findall(r"([^\w]de[^\w])|[^\w]d'", v)}
+    parsed_verses = parse_verses(kon_ru_fr)
+
+    NdeN_verses = {verse.text: [] for verse in parsed_verses}
+    for verse in parsed_verses:
+        for token in verse[:-3]:
+            constr = is_NdeN_nref(token, 
+                                  verse[token.i + 1],
+                                  verse[token.i + 2],
+                                  verse[token.i + 3])
+            if constr[0]:
+                NdeN_verses[verse.text].append(constr[1])
+    
+    NdeN_verses = {k: v for k, v in NdeN_verses.items() if v}
+
+    return filter_verses(kon_ru_fr, NdeN_verses)
+
+
+def get_cop_det_nref(text: str):
 
     kon_ru_fr = extract_verses(text)
     parsed_verses = parse_verses(kon_ru_fr)
@@ -54,28 +126,43 @@ def get_cop_det_nref(text):
     return filter_verses(kon_ru_fr, cop_det_verses)
 
 
-def get_NdeN_nref(text):
+with open('Parallel corpus/Konabere-Russian-French/United books/New Testament.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
 
-    kon_ru_fr = extract_verses(text)
-    kon_ru_fr = {k: v for k, v in kon_ru_fr.items() if re.findall(r"([^\w]de[^\w])|[^\w]d'", v)}
-    parsed_verses = parse_verses(kon_ru_fr)
+with open('Contexts/NdeN (nref).txt', 'w', encoding='utf-8') as f:
+    verses = get_NdeN_nref(text)
+    print(len(verses))
+    for verse in verses:
+        f.write(verse + '\n\n')
 
-    nom_de_nom = []
-    for verse in parsed_verses:
-        for idx, token in enumerate(verse):
-            w1 = token.head.head
-            w2 = token.head
-            if (token.pos_ == 'ADP' 
-                and token.text in ['de', "d'"] 
-                and w1.pos_ == 'NOUN' 
-                and w2.pos_ == 'NOUN'
-                and w1.i == idx - 1):
-                    if is_concrete(w1.lemma_, 'n', 'fra') and is_concrete(w2.lemma_, 'n', 'fra'):
-                        if (w2.i == idx + 1 
-                            and verse not in nom_de_nom):
-                            nom_de_nom.append(verse.text)
-                        elif (verse[idx + 1].lemma_ == 'un' 
-                                and verse not in nom_de_nom):
-                            nom_de_nom.append(verse.text)
+# kon_ru_fr = extract_verses(text)
+# kon_ru_fr = {k: v for k, v in kon_ru_fr.items() if re.findall(r"([^\w]de[^\w])|[^\w]d'", v)}
+# parsed_verses = parse_verses(kon_ru_fr)
 
-    return filter_verses(kon_ru_fr, nom_de_nom)
+# NdeN_verses = {verse.text: [] for verse in parsed_verses}
+# for verse in parsed_verses:
+#     for token in verse[:-3]:
+#         constr = is_NdeN_nref(token, 
+#                               verse[token.i + 1],
+#                               verse[token.i + 2],
+#                               verse[token.i + 3])
+#         if constr[0]:
+#             NdeN_verses[verse.text].append(constr[1])
+
+# NdeN_verses = {k: v for k, v in NdeN_verses.items() if v}
+
+# for k, v in NdeN_verses.items():
+#     print(k, v, '\n')
+
+# filtered = []
+# for k, v in kon_ru_fr.items():
+#     if v in NdeN_verses:
+#         highl_verse = k
+#         for constr in NdeN_verses[v]:
+#             highl_verse = highl_verse.replace(constr, constr.upper())
+#         filtered.append(highl_verse)
+
+# for verse in filtered:
+#     print(verse, '\n\n')
+            
+    
